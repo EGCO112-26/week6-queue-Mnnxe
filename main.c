cc1cd867ccea297d7f9eb6d9e8dbdd128519aa4a
Mnnxe <napatminnie@gmail.com>
2026-02-11 08:33:07 +0000

    assg 6

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int main(int argc , char **argv) {
         }
         else {
             enqueue_struct(&q, atoi(argv[i]));
-            printf("queing %d\n",atoi(argv[i]));
+            //printf("queing %d\n",atoi(argv[i]));
            
         }
  }
